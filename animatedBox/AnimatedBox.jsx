b8d77e5240607e60ede33c3d5f53c9016c85f0dc
Antonio Sánchez <asanchz85@gmail.com>
2024-11-07 20:08:14 +0100

    Add files via upload

---
+++ animatedBox/AnimatedBox.jsx
@@ -0,0 +1,13 @@
+import './animatedBox.scss'
+
+function AnimatedBox () {
+  return (
+    <div className='coins_elem__container'>
+      <div className='coins_box'>
+        <span>Coins</span>
+      </div>
+    </div>
+  )
+}
+
+export default AnimatedBox
